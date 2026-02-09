f05983ed686434f3c7e31afcc2146a7a48bcd103
Moshe Amar <moso2411@gmail.com>
2026-02-09 03:27:52 +0200

    chore: prepare repo for public push

--- apps/intake-service/src/index.ts
+++ apps/intake-service/src/index.ts
@@ -29,6 +29,15 @@ const start = async () => {
   app.locals.channel = channel;
 
   app.use(helmet());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+    next();
+  });
   app.use(express.json({ limit: '256kb' }));
   app.use(correlationIdMiddleware);
   app.use(
